3df8ff30f4ead5cee5be85f4561c5245669247d6
carl <zhongzheng.wang1986@gmail.com>
2015-03-31 10:46:32 +0800

    Symptom: add demo_time
    Bug ID:
    Root Cause:
    Solution:
    ChangeLists:

---
+++ py/demo_time.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#-*- encoding:utf-8 -*-
+
+import time
+
+'''
+python中时间日期格式化符号：
+%y 两位数的年份表示（00-99）
+%Y 四位数的年份表示（000-9999）
+%m 月份（01-12）
+%d 月内中的一天（0-31）
+%H 24小时制小时数（0-23）
+%I 12小时制小时数（01-12）
+%M 分钟数（00=59）
+%S 秒（00-59）
+
+%a 本地简化星期名称
+%A 本地完整星期名称
+%b 本地简化的月份名称
+%B 本地完整的月份名称
+%c 本地相应的日期表示和时间表示
+%j 年内的一天（001-366）
+%p 本地A.M.或P.M.的等价符
+%U 一年中的星期数（00-53）星期天为星期的开始
+%w 星期（0-6），星期天为星期的开始
+%W 一年中的星期数（00-53）星期一为星期的开始
+%x 本地相应的日期表示
+%X 本地相应的时间表示
+%Z 当前时区的名称
+%% %号本身
+'''
+
+# unix time stamp
+print time.time()
+
+# full time info
+print time.localtime(time.time())
+
+# formatted time
+print time.strftime('%Y-%m-%d',time.localtime(time.time()))
--- py/log_advanced/README.md
+++ py/log_advanced/README.md
@@ -1,6 +1,7 @@
 ###Configuring Logging
 
 Programmers can configure logging in 3 ways:
+
 1. Creating loggers, handlers, and formatters explicitly using Python code
 that calls the configuration methods
 2. Creating a logging config file and reading it using `fileConfig()` function
