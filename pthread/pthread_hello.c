fa8c179a9d7a0512d31db48b9783af92fdffed0c
carl <zhongzheng.wang1986@gmail.com>
2015-05-01 11:25:03 +0800

    Summary: Add comment
    Root Cause:
    Solution:
    Change Lists:

--- pthread/pthread_hello.c
+++ pthread/pthread_hello.c
@@ -20,6 +20,9 @@ int main(int argc, char **argv)
     long t;
     for (t=0; t<NUM_THREADS; t++) {
         printf("In main: creating thread %ld\n", t);
+
+        // 第4个参数，如果传&t这样的地址的话，那么在PrintHello里面
+        // 输出的tid会全部都一样，观察不到依次创建了5个线程
         rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
         if (0 != rc) {
             printf("ERROR, return code from pthread_create{} is %d\n", rc);
